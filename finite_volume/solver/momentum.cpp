12825c3fcc9aa627288151022ca92e988ae1ef46
Ailin Yu <axy98@case.edu>
2017-11-04 19:04:26 -0400

    Corrected velocity calculation loop sizes

--- finite_volume/solver/momentum.cpp
+++ finite_volume/solver/momentum.cpp
@@ -9,7 +9,7 @@ void Momentum::velocity_intermediate(double h, double mu, double dt, std::vector
 	//U projection matrix 
 	for(unsigned int x = 1; x < u.size()-1; x++)
 	{
-		for(unsigned int y = 1; y < u[x].size()-1; y++)
+		for(unsigned int y = 1; y < u[x].size()-2; y++)
 		{
 		return_u[x][y] = u[x][y] + dt*((-0.25/h)*(pow((u[x-1][y]+u[x][y]),2.0) 
 					- pow((u[x+1][y]+u[x][y]),2.0) 
@@ -18,14 +18,14 @@ void Momentum::velocity_intermediate(double h, double mu, double dt, std::vector
 				+ (mu/pow(h,2.0))*(u[x-1][y]+u[x+1][y]+u[x][y-1]+u[x][y+1]
 					-(4*u[x][y])));	
 
-/*		}	
+		}	
 	}	
 
 	//V projection matrix
-	for(unsigned int x = 1; x < u.size()-1; x++)
+	for(unsigned int x = 1; x < v.size()-2; x++)
 	{
-		for(unsigned int y = 1; y < u[x].size()-1; y++)
-		{ */
+		for(unsigned int y = 1; y < v[x].size()-1; y++)
+		{ 
 		return_v[x][y] = v[x][y] + dt*(-(0.25/h)*((u[x][y-1]+u[x][y])
 					*(v[x-1][y]+v[x][y]) - (u[x+1][y-1]+u[x+1][y])
 					*(v[x+1][y]+v[x][y]) + (pow((v[x][y-1]+v[x][y]),2.0)
@@ -45,9 +45,17 @@ for(unsigned int x = 1; x < u.size()-1; x++)
 	for(unsigned int y = 1; y < u[x].size()-1; y++)
 	{
 		u[x][y] = u_temp[x][y] - (dt/h)*(-p[x+1][y] + p[x][y]);
+	}
+}
+
+for(unsigned int x = 1; x < v.size()-1; x++)
+{
+	for(unsigned int y = 1; y < v[x].size()-1; y++)
+	{
 		v[x][y] = v_temp[x][y] - (dt/h)*(-p[x][y+1] + p[x][y]); 
 	}
 }
+
 
 }
 
