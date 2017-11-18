9bd3399f6deafc85e919f4e6e297ba8aac6c9d96
Ailin Yu <axy98@case.edu>
2017-11-18 14:18:15 -0500

    Added circle to RE20 benchmark sim

--- finite_volume/solver/sim.cpp
+++ finite_volume/solver/sim.cpp
@@ -1,5 +1,6 @@
 #include "../include/sim.h"
 #include "../include/momentum.h"
+#include "../include/ignore.h"
 #include "../include/boundary.h"
 #include "../include/pressure.h"
 #include "../include/writetocsv.h"
@@ -39,16 +40,32 @@ void Simulation::cylinderRe20(int timesteps)
 {
 	
 	//initialize objects
-	Initialization init(18,18, 0, 0, 0, 0);
+	Initialization init(36,36, 0, 0, 0, 0);
 	Momentum velocitystep;
 	Boundary boundary;
 	Pressure pressurestep;
+	Ignore object;
 	WriteToCSV writer;
 
+	//delete previous versions of files
 	writer.deletefile("udata_RE20");
 	writer.deletefile("vdata_RE20");
 	writer.deletefile("pdata_RE20");
+
 	
+	double radius = 1.0;
+	double xcenter = 3.0;
+	double ycenter = 8.0;
+	//populate ignore vector
+
+	for(unsigned int x = 0; x < (init.getU()).size(); x++)
+	{
+		for(unsigned int y = 0; y < (init.getV())[x].size(); y++)
+		{
+			object.circle(radius, xcenter, ycenter, x, y, init.getXignore(), init.getYignore(), init.getXedges(), init.getYedges());
+		}
+	}	
+
 
 	//run simulation
 	for(int i = 0; i < timesteps; i++)
